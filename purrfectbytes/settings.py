60823248f28734d4e849fc93f456c69520371464
Alina Zhdanova <zhdanova.allina@yandex.ru>
2024-05-26 01:38:37 +0300

    Users added

--- purrfectbytes/settings.py
+++ purrfectbytes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'users.apps.UsersConfig',
     'orders.apps.OrdersConfig',
 ]
 
@@ -130,4 +131,6 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CART_SESSION_ID = 'cart'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
