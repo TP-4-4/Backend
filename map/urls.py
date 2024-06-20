d4cdeeaa044f5b09ff141b7bd411a9aaf2f60637
Alina Zhdanova <zhdanova.allina@yandex.ru>
2024-06-20 05:25:05 +0300

    Cosmetic changes again

--- map/urls.py
+++ map/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 from . import views
+from .views import MapView
 
 app_name = 'map'
 
 urlpatterns = [
     path('', views.index, name='index'),  # Путь к главной странице
-    path('get_route_data/<order_id>/', views.get_route_data, name='get_route_data'),
+    path('get_route_data/<order_id>/', MapView.as_view(), name='get_route_data'),
 ]
